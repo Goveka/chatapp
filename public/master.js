19cd279bb2ecf86ed59ab80c6bf0d8ef8e45ef75
unknown <sizwenkala80@gmail.com>
2022-04-23 18:14:26 +0200

    make it better

---
+++ public/master.js
@@ -0,0 +1,24 @@
+const socket=io();
+let messages=document.getElementById("messages")
+let form=document.getElementById('form');
+let input=document.getElementById('input')
+
+
+form.addEventListener('click', emaen)
+
+function emaen(hello) {
+  hello.preventDefault();
+  if(input.value){
+    socket.emit('chat message', input.value);
+    input.value='';
+}
+}
+
+
+socket.on('chat message', function (msg) {
+let item=document.createElement('li');
+item.textContent=msg;
+messages.appendChild(item);
+window.scrollTo(0, document.body.scrollHeight);
+
+})
